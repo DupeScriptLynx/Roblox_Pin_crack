83ed168d6847ed032ed52c71dfafa93fd3476c92
DupeScript <118762902+DupeScriptLynx@users.noreply.github.com>
2024-10-13 09:24:33 +0300

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,15 @@
+@echo off
+:start
+pip install cryptography
+pip install opencv-python
+pip install psutil
+pip install pycountry
+pip install pyperclip
+pip install pypiwin32
+pip install requests
+pip install requests-toolbelt
+pip install wmi
+pip install pyaesm
+pip install browser-cookie3
+pause
+exit
